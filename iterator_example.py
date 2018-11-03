3501ca6d5730070e7a1bc32c34b6c2cd40333193
cphadkule <cphadkule@gmail.com>
2018-11-03 20:20:58 +0530

    To create a Python iterator object, you will need to implement two methods in your iterator class.

---
+++ iterator_example.py
@@ -0,0 +1,25 @@
+import random
+
+class CoolEmoticonGenerator(object):
+    """docstring for CoolEmoticonGenerator."""
+
+    strings = "!@#$^*_-=+?/,.:;~"
+    grouped_strings = [("(", ")"), ("<", ">"), ("[", "]"), ("{", "}")]
+
+    def create_emoticon(self, grp):
+        """actual method that creates the emoticon"""
+        face_strings_list = [random.choice(self.strings) for _ in range(3)]
+        face_strings = "".join(face_strings_list)
+        emoticon = (grp[0], face_strings, grp[1])
+        emoticon = "".join(emoticon)
+        return emoticon
+
+    def __iter__(self):
+        """returns the self object to be accessed by the for loop"""
+        return self
+
+    def __next__(self):
+        """returns the next emoticon indefinitely"""
+        grp = random.choice(self.grouped_strings)
+        return self.create_emoticon(grp)
+
